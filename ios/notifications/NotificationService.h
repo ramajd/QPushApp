cc6ff56d6efba542b88f158b2703645a269aa72e
Reza Alizadeh Majd <r.majd@pantherx.org>
2023-07-08 11:26:06 +0330

    add notification service extension related files

---
+++ ios/notifications/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  notifications
+//
+//  Created by Reza Alizadeh Majd on 7/8/23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
